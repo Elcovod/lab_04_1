792463d0b112a1a46a1cdbebae4ed2aa592c3f22
Yurii <kanyukayurij@gmail.com>
2025-09-26 19:26:24 +0300

    lab

--- lab_04_1.cpp
+++ lab_04_1.cpp
@@ -7,20 +7,49 @@ int main()
 {
 	int N;
 	double S;
+
 	double C;
 
 	cout << "N (> 0) = "; cin >> N;
 
 	S = 0;
-	
-	for (int k = 1; k <= N; k++) { //цикл
+	int k = 1;
+	while ( k <= N)
+	{
+		C = pow((2.0 * k + 1.0), 2);
+		S += 1.0 / C;
+		++k;
 
-		C = pow((2.0 * k + 1.0), 2); //знаменник
+	}
+	cout << S << endl;
+
+	k = 1;
+	S = 0;
+	do {
+		C = pow((2.0 * k + 1.0), 2);
+		S += 1.0 / C;
+		++k;
+	} while (k <= N);
+	cout << S << endl;
+
+	S = 0;
+	for (int k = 1; k <= N; k++) {
+
+		C = pow((2.0 * k + 1.0), 2);
 		S += 1.0 / C;
 
 	}
+	cout << S << endl;
+
+	S = 0;
+	for (int k = N; k >=1; k--) {
+
+		C = pow((2.0 * k + 1.0), 2);
+		S += 1.0 / C;
+
+	}
+	cout << S << endl;
 
 	cout << fixed << setprecision(8);
-	cout << "Sum for N = " << N << " equals " << S << endl;
 	cin.get();
 }
